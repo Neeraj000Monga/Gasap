bf84b0076f82b05a18801340a6e055f3f233473f
neeraj monga <neerajmonga000@gmail.com>
2023-10-31 18:01:58 +0530

    first commi

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+// import introvideo from "./video/pexels_videos_2757709 (1080p).mp4";
+
+gsap.registerPlugin(ScrollTrigger);
+
+// interface AppProps {}
+// interface AppState {
+//   name: string;
+// }
+
+export default function App(props) {
+  const number = useRef(null);
+
+  useEffect(() => {
+    gsap.to("#h1", {
+      scrollTrigger: {
+        trigger: "#header",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+      yPercent: 50,
+      scale: 3,
+      opacity: 0,
+    });
+    // Section 1 H2
+    gsap.from("#h2", {
+      scrollTrigger: {
+        trigger: "#h2",
+        start: "top bottom",
+        end: "top 400px",
+        // start: "top",
+        // end: 'bottom',
+        scrub: 1,
+        // toggleActions: "play complete none reset"
+      },
+      xPercent: -100,
+      opacity: 0,
+    });
+    // Execution heading
+    gsap.from("#h3", {
+      scrollTrigger: {
+        trigger: "#h3",
+        start: "top bottom+=100px",
+        // scrub: true
+        toggleActions: "play complete none reset",
+      },
+      xPercent: 100,
+      opacity: 0.5,
+      duration: 1 
+    });
+    // Custom trigger
+    ScrollTrigger.create({
+      trigger: "#h3",
+      start: "top bottom+=-200px", // 200px after the top passes the bottom of the viewport
+      endTrigger: '#section2',
+      end: "bottom top",
+      onUpdate: (self) => {
+        const progress = Math.max(2, Math.ceil(self.progress * 100)); //No lower than 2.
+        number.current.innerHTML = progress;
+        // console.log(
+        //   "progress:",
+        //   self.progress.toFixed(3),
+        //   "direction:",
+        //   self.direction,
+        //   "velocity",
+        //   self.getVelocity()
+        // );
+      },
+    });
+  
+    ScrollTrigger.refresh()
+
+    
+  }, []);
+
+  return (
+    <main>
+      {/* <video>
+      <source src={introvideo}  type="Video/webm" />
+      </video> */}
+      <header id="header">
+        <h1 id="h1">Execution Is Everything</h1>
+      </header>
+      <section>
+        <h2 id="h2">The idea is step 1.</h2>
+      </section>
+      <section id="section2">
+        <h2 id="h3">
+          Execution is steps <span ref={number}>2</span>.
+        </h2>
+      </section>
+    </main>
+  );
+}
+
+
